367b1fe5d08bdae3d8fb562d4c1c7cda5ab23374
duserc <danielcollison@hotmail.co.uk>
2023-06-10 19:21:46 +0100

    rage delete old archaic code

--- mainForm/mainForm/SoundSwapMainForm.cs
+++ mainForm/mainForm/SoundSwapMainForm.cs
@@ -9,7 +9,6 @@ using NAudio.Wave;
 using WK.Libraries.HotkeyListenerNS;
 using static audioDeviceLibrary.audioDevices;
 
-//using WK.Libraries.HotkeyListenerNS;
 
 public partial class mainForm : Form
 {
@@ -17,7 +16,7 @@ public partial class mainForm : Form
 
     public mainForm()
     {
-        InitializeComponent();
+        //InitializeComponent();
         listOfSoundDevices = new List<SoundDevice>();
         PopulateAudioDeviceData();
     }
@@ -48,59 +47,6 @@ public partial class mainForm : Form
                     listOfSoundDevices.Add(newSoundDev);
                 }
             }
-            //newSoundDevices.Add(soundDevice);
-            ////NEED TO WRITE CODE TO READ ACTIVE AND KEYBIND STATE HERE
-            //DataRow newRow = audioDeviceTable.NewRow();
-            //newRow["Audio Device"] = audioDevice;
-            //newRow["Active"] = false;
-            //audioDeviceTable.Rows.Add(newRow);
-        }
-        myDataGridView.RowHeadersVisible = false;
-        myDataGridView.DataSource = audioDeviceTable;
-
-        DataGridViewButtonColumn hotkeyColumn = new DataGridViewButtonColumn();
-        hotkeyColumn.Name = "customHotkey";
-        hotkeyColumn.HeaderText = "Custom Hotkey";
-        hotkeyColumn.Text = "Unbound";
-        hotkeyColumn.UseColumnTextForButtonValue = true;
-        myDataGridView.Columns.Add(hotkeyColumn);
-        myDataGridView.Columns["customHotkey"].DisplayIndex = audioDeviceTable.Columns.Count;
-        myDataGridView.Columns["Audio Device"].SortMode = DataGridViewColumnSortMode.NotSortable;
-        myDataGridView.Columns["Audio Device"].Resizable = DataGridViewTriState.False;
-        myDataGridView.Columns["Audio Device"].ReadOnly = true;
-        myDataGridView.Columns["Audio Device"].Frozen = true;
-        myDataGridView.AllowUserToResizeColumns = false;
-        myDataGridView.AllowUserToResizeRows = false;
-        myDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-
-    }
-
-    private void InitializeComponent()
-    {
-
-    }
-
-    private void DataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
-    {
-        // if button clicked
-        if (e.ColumnIndex == myDataGridView.Columns["customHotkey"].Index &&
-            e.RowIndex >= 0 && e.RowIndex < myDataGridView.Rows.Count)
-        {
-            // access hotkey library, binding new hotkey
-
-
-            // just create new form to recieve hotkey inputs
-
-            SoundDevice soundDevice = newSoundDevices[e.RowIndex];
-            //soundDevice.HotKey = hotKeyHandler.CreateBind();
-            MessageBox.Show("You entered: " + e.ColumnIndex);
-            HotKeyForm frm = new HotKeyForm();
-            frm.Show();
-
-            //string input = ShowInputDialog("Choose hotkey:", "Input Box");
-
-
         }
     }
-
 }
