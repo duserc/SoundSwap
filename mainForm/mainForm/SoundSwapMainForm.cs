925af5d911bb58105c1184e4b93e54478f76b82e
duserc <danielcollison@hotmail.co.uk>
2023-06-10 19:16:24 +0100

    removed archaic comments

--- mainForm/mainForm/SoundSwapMainForm.cs
+++ mainForm/mainForm/SoundSwapMainForm.cs
@@ -24,13 +24,8 @@ public partial class mainForm : Form
 
     private void PopulateAudioDeviceData()
     {
-        //myDataGridView.CellContentClick += DataGridView_CellContentClick;
-        //DataTable audioDeviceTable = new DataTable();
-        //audioDeviceTable.Columns.Add("Audio Device", typeof(string));
-        //audioDeviceTable.Columns.Add("Active", typeof(bool));
-
         //getting the name of each audio device plugged into PC
-        foreach (AudioDeviceInfo audioDevice in audioDevices.EnumberateAudioDevices())
+        foreach (AudioDeviceInfo audioDevice in EnumberateAudioDevices())
         {
             //Looping over the SoundDevices inside the Settings.json
             foreach (SoundDevice SoundDevice in ListManager.ListDatabase)
@@ -41,19 +36,15 @@ public partial class mainForm : Form
                     //if so:
                     listOfSoundDevices.Add(SoundDevice);
                 }
-                //This below code will not work because it would just keep adding new sound devices
-                //else
-                //{
-                //    SoundDevice newSoundDevice = new SoundDevice(audioDevice, false, false, );
-                //}
             }
             //Looping over every sound device in new list with updated settings
             foreach (SoundDevice SoundDevice in listOfSoundDevices)
             {
-                //if the current sound device is not in new list, it means its probably first launch
+                //if the current sound device is not in new list, it means its probably first launch or new audio device
                 if (!listOfSoundDevices.Contains(SoundDevice))
                 {
-                    SoundDevice newSoundDev = new SoundDevice(SoundDevice.AudioDevice, false, audioDevice.IsDefaultDevice, )
+                    //creating new sound device with defualt properties and adding it to the list of sound devices.
+                    SoundDevice newSoundDev = new SoundDevice(SoundDevice.AudioDevice, false, audioDevice.IsDefaultDevice, null);
                     listOfSoundDevices.Add(newSoundDev);
                 }
             }
